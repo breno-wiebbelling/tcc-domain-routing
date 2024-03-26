546f309282068cf96b52b841e398ae7d563b6709
Breno Wiebbelling <brenowdea@gmail.com>
2024-03-25 21:31:28 -0300

    gitignore

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const app = express();
 
-const originalRouter = express.Router();
 const userCustomRouter = express.Router();
 const defaultRouter = express.Router();
-
-originalRouter.get('/', (req, res) => {
-  res.send('Hello from originalAppName.com');
-});
 
 userCustomRouter.get('/', (req, res) => {
   res.send('Hello from originalAppName.userCustomRouter.com');
@@ -19,19 +14,17 @@ defaultRouter.get('/', (req,res) => {
 
 app.use((req, res, next) => {
   const parts = req.hostname.split('.');
-  console.log(parts)
-  const subdomain = parts[0];
+  const subdomain = parts[parts.length-2];
 
   switch (subdomain) {
-    case 'breno':
-      return originalRouter(req, res, next);
-    case 'usercustom':
+    case 'routes':
       return userCustomRouter(req, res, next);
     default:
       return defaultRouter(req, res, next);
   }  
 });
 
-app.listen(8080, () => {
-  console.log(`Server is running on port`);
+let SERVER_PORT = 8090;
+app.listen(8090, () => {
+  console.log(`Server is running on port ${SERVER_PORT}`);
 });
